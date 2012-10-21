03d8e8816e86bd7eef65a65a45a11ab0f1da1ab4
Chris Taylor <crntaylor@gmail.com>
2012-10-21 11:31:02 +0100

    expand, collect and simplify terms

--- Simplify.hs
+++ Simplify.hs
@@ -4,17 +4,23 @@ module Simplify where
 
 import Control.Monad (sequence)
 
-import Expr
 import Symbolic
 
+-- |Simplify expressions.
+simplify :: (SymbolicExpr a) => a -> a
+simplify = collect . expand
+
 -- |Expand out all brackets, distributing multiplication over addition.
-expand :: (SymbolicSum a, SymbolicProd a, SymbolicDiv a) => a -> a
-expand (sumD  -> Just as)   = sumC (map expand as)
-expand (prodD -> Just as)   =
+expand :: (SymbolicExpr a) => a -> a
+expand (sumD  -> Just as)       = sumC (map expand as)
+expand (prodD -> Just as)       =
     let sums = map (\(sumD -> Just a) -> a) $ filter isSum as
         rest = map return $ filter (not . isSum) as
-
     in  sumC $ map prodC $ sequence (rest ++ sums)
-expand (divD -> Just (a,b)) = divC (expand a) (expand b)
-expand other                = other
+expand (divD -> Just (a,b))     = divC (expand a) (expand b)
+expand (funD -> Just (str,f,a)) = funC str f (expand a)
+expand other                    = other
 
+-- |Collect like terms.
+collect :: (SymbolicExpr a) => a -> a
+collect other = other
