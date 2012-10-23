e63306b666630402c60f9a790cabd95427c1d848
Chris Taylor <crntaylor@gmail.com>
2012-10-23 07:03:32 +0100

    latex expressions

--- Latex.hs
+++ Latex.hs
@@ -1,19 +1,63 @@
 module Latex where
 
 import Expr
+import qualified Data.Set as Set
+import qualified Data.Map as Map
+import qualified Data.List as List
 
 type Latex = String
 
-latex :: Expr -> Latex
-latex expr = "\\begin{equation}\n" ++ go expr ++ "\n\\end{equation}\n"
+latexAtom :: Atom -> Latex
+latexAtom (Var s)   = s
+latexAtom (App f s) = escape f ++ "(" ++ show s ++ ")"
+
+latexProd :: Prod -> String
+latexProd (Prod m) = List.intercalate " " $ map shw $ Map.toList m
     where
-        go (Num n) = show n
-        go (Var v) = v
-        go (Neg a) = "-" ++ go a
-        go (Add a b) = "(" ++ go a ++ " + " ++ go b ++ ")"
-        go (Sub a b) = "(" ++ go a ++ " - " ++ go b ++ ")"
-        go (Mul a b) = "(" ++ go a ++ " " ++ go b ++ ")"
-        go (Div a b) = "\\frac{" ++ go a ++ "}{" ++ go b ++ "}"
-        go (Pow a n) = "(" ++ go a ++ ") ^ { " ++ show n ++ "}"
-        go (Lit f a) = f ++ "(" ++ go a ++ ")"
-        go (App f _ a) = "\\" ++ f ++ "(" ++ go a ++ ")"
+        shw (e,n) = latexAtom e ++ (case n of
+                                    1 -> ""
+                                    2 -> "^2"
+                                    3 -> "^3"
+                                    4 -> "^4"
+                                    5 -> "^5"
+                                    6 -> "^6"
+                                    7 -> "^7"
+                                    8 -> "^8"
+                                    9 -> "^9"
+                                    n -> "^{" ++ show n ++ "}")
+
+latexExpr :: Expr -> String
+latexExpr (Expr m) =
+    if Map.null m
+        then "0"
+        else List.intercalate " + " . map shw $ Map.toList m
+    where
+        shw (p,n) = pre ++ latexProd p
+            where
+                pre = if isConstP p
+                        then show n
+                        else case n of
+                                1    -> ""
+                                (-1) -> "-"
+                                _    -> show n
+
+escape :: String -> Latex
+escape f = if f `Set.member` latexFunctions then '\\' : f else f
+
+latexFunctions = Set.fromList
+    [ "sin"
+    , "cos"
+    , "tan"
+    , "asin"
+    , "acos"
+    , "atan"
+    , "sinh"
+    , "cosh"
+    , "tanh"
+    , "asinh"
+    , "acosh"
+    , "atanh"
+    , "exp"
+    , "sqrt"
+    , "log"
+    ]
