829d1dbc3fe81f5010e15c77d4bc65b0cded7633
Chris Taylor <crntaylor@gmail.com>
2012-10-27 00:26:40 +0100

    Trio data type

---
+++ Trio.hs
@@ -0,0 +1,8 @@
+module Trio where
+
+data Trio u v w = First u | Second v | Third w deriving (Eq,Ord,Show)
+
+trio :: (u -> a) -> (v -> a) -> (w -> a) -> Trio u v w -> a
+trio f _ _ (First a)  = f a
+trio _ g _ (Second a) = g a
+trio _ _ h (Third a)  = h a
