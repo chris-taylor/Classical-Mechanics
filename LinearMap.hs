5cf0b21538d26fcb695b7d070b68590e786bfd47
Chris Taylor <crntaylor@gmail.com>
2012-10-27 19:01:30 +0100

    Additive group and vector space instances

--- LinearMap.hs
+++ LinearMap.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE TypeOperators, TypeFamilies #-}
 
 --module LinearMap ( (:->), lapply, linear, Basis(..), VectorSpace (..) ) where
-module LinearMap ( (:->), lapply, Basis(..), VectorSpace(..) ) where
+module LinearMap ( (:->), lapply, idL, compose, lMap, unLMap, jsum, Basis(..), VectorSpace(..) ) where
 
 import Basis
 import VectorSpace
@@ -11,6 +11,8 @@ type MSum a = Maybe (Sum a)
 type LMap' u v = MSum (Basis u -> v)
 
 newtype u :-> v = LMap { unLMap :: LMap' u v }
+
+lMap = LMap
 
 atZ :: (AdditiveGroup b) => (a -> b) -> (MSum a -> b)
 atZ f = maybe zeroV (f . getSum)
@@ -44,3 +46,31 @@ compose :: (HasBasis a, Scalar a ~ Scalar b,
         -> (a :-> b)
         -> (a :-> c)
 compose f g = linear (lapply f . lapply g)
+
+---------- Linear maps are vector spaces?
+
+instance (HasBasis u, VectorSpace v,
+          Scalar u ~ Scalar v) => AdditiveGroup (u :-> v) where
+
+    zeroV = LMap Nothing
+
+    LMap Nothing <+> b            = b
+    a            <+> LMap Nothing = a
+    a            <+> b            = linear (\u -> lapply a u <+> lapply b u)
+
+instance (HasBasis u, VectorSpace v,
+          Scalar u ~ Scalar v) => VectorSpace (u :-> v) where
+    
+    type Scalar (u :-> v) = Scalar v
+
+    s *> LMap Nothing = LMap Nothing
+    s *> a            = linear (\u -> s *> lapply a u)
+
+    toList = undefined
+    fromList = undefined
+
+----------
+
+liftMS :: (a -> b) -> MSum a -> MSum b
+liftMS = fmap . fmap
+
