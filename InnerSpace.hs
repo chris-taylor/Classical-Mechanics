b8720c6335ecb25a9f8dbd12a64e97d79c4a63c7
Chris Taylor <crntaylor@gmail.com>
2012-10-27 17:34:29 +0100

    magnitude and normalize functions, tuple instances

--- InnerSpace.hs
+++ InnerSpace.hs
@@ -1,10 +1,24 @@
-module InnerSpace ( InnerSpace(..) ) where
+{-# LANGUAGE TypeFamilies, FlexibleContexts #-}
+
+module InnerSpace ( InnerSpace(..), (<.>), magnitudeSq, magnitude, normalized ) where
 
 import VectorSpace
 
 -- |This class describes spaces with an inner product.
 class (VectorSpace v) => InnerSpace v where
     dot :: v -> v -> Scalar v
+
+(<.>) :: (InnerSpace v) => v -> v -> Scalar v
+(<.>) = dot
+
+magnitudeSq :: (InnerSpace v) => v -> Scalar v
+magnitudeSq v = dot v v
+
+magnitude :: (InnerSpace v, Floating (Scalar v)) => v -> Scalar v
+magnitude = sqrt . magnitudeSq
+
+normalized :: (InnerSpace v, Floating (Scalar v)) => v -> v
+normalized v = v </ magnitude v
 
 -- Primitive instances
 
@@ -22,3 +36,11 @@ instance InnerSpace Double where
 
 -- Tuple instances
 
+instance (InnerSpace u, InnerSpace v, Scalar u ~ Scalar v,
+          AdditiveGroup (Scalar u)) => InnerSpace (u,v) where
+    dot (a,b) (c,d) = dot a c <+> dot b d
+
+instance (InnerSpace u, Scalar u ~ Scalar v,
+          InnerSpace v, Scalar v ~ Scalar w,
+          InnerSpace w, AdditiveGroup (Scalar w)) => InnerSpace (u,v,w) where
+    dot (a,b,c) (d,e,f) = dot a d <+> dot b e <+> dot c f
